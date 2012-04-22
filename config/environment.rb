1ebf94ee4f6a5236b5127377308d916b6d3fe838
Alexander Nelson <Alex@Hemraketmaskin.(none)>
2012-04-22 18:26:56 +0200

    User signup complete

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 SampleApp::Application.initialize!
+
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  errors = Array(instance.error_message).join(',')
+  %(#{html_tag}<span class="validation-error">&nbsp;#{errors}</span>).html_safe
+end
